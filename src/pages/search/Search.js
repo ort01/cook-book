eecfcf78a36b73771d53a2275751fdf466e854f1
ort01 <ortutova.kristina@gmail.com>
2023-11-10 18:09:02 +0100

    getting the value of the custom query parameter

--- src/pages/search/Search.js
+++ src/pages/search/Search.js
@@ -1,6 +1,15 @@
 import "./Search.scss"
 
+import { useLocation } from 'react-router-dom'
+
+
 export default function Search() {
+
+    // getting the value of the custom query parameter -> ?q=value (note: q= can also be dog=, mother= or whatever)
+    const queryString = useLocation() //?q="xy"
+    const queryParams = new URLSearchParams(queryString)
+    const query = queryParams.get('q')
+
     return (
         <div>Search</div>
     )
