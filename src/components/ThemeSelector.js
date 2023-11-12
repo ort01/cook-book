1f248d95d04b6ec3b2443b5acea1f54c959faea0
ort01 <ortutova.kristina@gmail.com>
2023-11-12 18:44:43 +0100

    Light/Dark mode selector

--- src/components/ThemeSelector.js
+++ src/components/ThemeSelector.js
@@ -1,22 +1,42 @@
 import React from 'react'
 import "./ThemeSelector.scss"
 import { useThemeContext } from "../hooks/useThemeContext"
+import modeIcon from "../assets/mode-icon.svg"
 
 export default function ThemeSelector() {
 
-    const { changeColor } = useThemeContext()
+    const { changeColor, changeMode, mode } = useThemeContext()
+
     const themeColors = ["#4297a0", "#e4b7a0", "#A49393"]
 
+    const toggleMode = () => {
+
+        changeMode(mode === "light" ? "dark" : "light")
+        console.log(mode);
+    }
+
     return (
-        <div className='theme-selector'>
-            {themeColors.map((color) => (
-                <div
-                    className="theme-selector__color"
-                    style={{ background: color }}
-                    onClick={() => {
-                        changeColor(color)
-                    }} />
-            ))}
+        <div className='selector'>
+            <div className='selector__theme'>
+                {themeColors.map((color) => (
+                    <div
+                        key={color}
+                        className="selector__theme--color"
+                        style={{ background: color }}
+                        onClick={() => {
+                            changeColor(color)
+                        }} />
+                ))}
+            </div>
+            <div className="selector__mode">
+                <img
+                    src={modeIcon}
+                    alt="mode-icon"
+                    onClick={toggleMode}
+                    style={{ filter: mode === "light" ? "invert(100%)" : null }}
+                />
+            </div>
         </div>
+
     )
 }
--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -6,8 +6,10 @@ export const ThemeContext = createContext() //this returns an object
 const themeReducer = (state, action) => { // two arguments: 1. current up to date state, 2. action object from dispatch
     //returning a value that represents the new state
     switch (action.type) {
-        case 'COLOR_CHANGE':
+        case 'CHANGE_COLOR':
             return { ...state, color: action.payload }
+        case 'CHANGE_MODE':
+            return { ...state, mode: action.payload }
         default:
             return state
     }
@@ -18,18 +20,25 @@ const themeReducer = (state, action) => { // two arguments: 1. current up to dat
 export function ThemeContextProvider({ children }) { // children prop represents any children COMPONENTS that the THemeProvider component wraps
 
 
-    const [state, dispatch] = useReducer(themeReducer, { color: "#4297a0" }) // 1. argument - reducerFunction for updating the state; 2.argument - initial state
+    const [state, dispatch] = useReducer(themeReducer, { // 1. argument - reducerFunction for updating the state; 2.argument - initial state
+        color: "#4297a0",
+        mode: "light"
+    })
 
     const changeColor = (color) => {
         //dispatch - is a way that we can dispatch (send) state change to the reducer function (themeReducer)
-        dispatch({ type: "COLOR_CHANGE", payload: color }) // argument is an action object with changed state (type of change, change itself - payload)
+        dispatch({ type: "CHANGE_COLOR", payload: color }) // argument is an action object with changed state (type of change, change itself - payload)
+    }
+
+    const changeMode = (mode) => {
+        dispatch({ type: "CHANGE_MODE", payload: mode })
     }
 
 
 
     return (
         //passing in the spreaded STATE object with function that can change the state
-        < ThemeContext.Provider value={{ ...state, changeColor }}>
+        < ThemeContext.Provider value={{ ...state, changeColor, changeMode }}>
             {children}
         </ThemeContext.Provider>
     )
